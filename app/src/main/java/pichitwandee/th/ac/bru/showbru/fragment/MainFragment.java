a8891895f8f20504b84a27cc808360e4341afbd6
pichitw <pichit.wandee@gmail.com>
2018-04-25 16:28:42 +0700

    Replace Fragment

--- app/src/main/java/pichitwandee/th/ac/bru/showbru/fragment/MainFragment.java
+++ app/src/main/java/pichitwandee/th/ac/bru/showbru/fragment/MainFragment.java
@@ -6,14 +6,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import pichitwandee.th.ac.bru.showbru.R;
 
 /**
- * Created by CS-BRU on 25/4/2561.
+ * Created by PChit-BRU on 25/4/2561.
  */
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    // Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
+    }   //Main Method
 
     @Nullable
     @Override
---
+++ app/src/main/java/pichitwandee/th/ac/bru/showbru/fragment/RegisterFragment.java
@@ -0,0 +1,24 @@
+package pichitwandee.th.ac.bru.showbru.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pichitwandee.th.ac.bru.showbru.R;
+
+/**
+ * Created by CS-BRU on 25/4/2561.
+ */
+
+public class RegisterFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        return  view;
+    }
+}
